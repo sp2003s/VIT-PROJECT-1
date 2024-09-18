d3c7d5c0af897c8666de447efe297b39ec970398
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-09-18 23:34:46 +0530

    Starting frontend for index page adding navbar

--- Application/settings.py
+++ Application/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, STATIC_URL),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 # Default primary key field type
