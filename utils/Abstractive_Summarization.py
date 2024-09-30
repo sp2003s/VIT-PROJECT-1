9f420e7ab6ddcb86cd6e7e0caaa0cfacdbc86558
Harsh-agrawal369 <agrawalharsh831@gmail.com>
2024-10-01 00:25:04 +0530

    Abstractive added update

--- utils/Abstractive_Summarization.py
+++ utils/Abstractive_Summarization.py
@@ -4,10 +4,10 @@
 
 from transformers import pipeline
 
-def abstractive_text_summarization(text):
+def abstractive_text_summarization(text, number_of_Pages):
     
     summarizer = pipeline("summarization", model="t5-base", tokenizer="t5-base")
-    summary = summarizer(text, max_length=150, min_length=40, do_sample=False)
+    summary = summarizer(text, max_length=150*number_of_Pages, min_length=40*number_of_Pages, do_sample=False)
 
     
     return summary
